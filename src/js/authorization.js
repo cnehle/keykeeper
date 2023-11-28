b76f0be51c1684aa7f325a2ebab95910343a9186
chertopol0h <kniko@sfedu.ru>
2023-11-28 20:28:51 +0300

    added curator authorization function

--- src/js/authorization.js
+++ src/js/authorization.js
@@ -1,9 +1,22 @@
-function checkAccessCode() {
+//функция для проверки пароля админа
+function checkAccessCodeAdmin() {
     var accessCode = document.getElementById("accessCodeInput").value;
     if (accessCode === "admin")
     {
     document.getElementById("message").innerText = "Доступ разрешён! Добро пожаловать, администратор.";
     // тут будет переход на страничку админа
+    }
+    else
+    document.getElementById("message").innerText = "Неверный код доступа";
+}
+
+//функция для проверки пароля куратора
+function checkAccessCodeCurator() {
+    var accessCode = document.getElementById("accessCodeInput").value;
+    if (accessCode === "curator")
+    {
+    document.getElementById("message").innerText = "Доступ разрешён! Добро пожаловать, куратор.";
+    // тут будет переход на страничку куратора
     }
     else
     document.getElementById("message").innerText = "Неверный код доступа";
