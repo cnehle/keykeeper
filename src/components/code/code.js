d70bf55c3645fe9307cce872db4f5200e19c6eb6
chertopol0h <kniko@sfedu.ru>
2024-04-03 22:51:01 +0300

    added script for access code check

--- src/components/code/code.js
+++ src/components/code/code.js
@@ -1,15 +1,22 @@
 
 import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import '../code/code.css';
 import imgSrc1 from './img/code.jpg';
 
 function Code() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const onNextClick = useCallback(() => {
-    navigate('/admin');
-  }, [navigate]);
+    const accessCode = document.getElementsByClassName("input")[0].value;
+    const btnId = location.state;
+    if (acceessCode === "admin" && btnId === "0") {
+      navigate('/admin');
+    } else {
+      alert("Некорректный пароль");
+    }
+  }, [navigate, location.state]);
 
   return (
     <div className="code">
