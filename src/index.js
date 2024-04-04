4fec4d5f2139f2d2baa05570f0f3e2fcc1c9e8fb
_cnehle <emoskvina@sfedu.ru>
2024-04-04 03:39:06 +0300

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/app'
 import './index.css';
+import { HashRouter } from 'react-router-dom';
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-      <App/>
-  </React.StrictMode> ,
-  document.getElementById('root')
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
