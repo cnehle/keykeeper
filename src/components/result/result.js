77c63b2dfae7b64a14dc710b5e7628f79d031932
AnyaKara <karapuzova@sfedu.ru>
2024-05-06 14:31:45 +0300

    Updated result.js

--- src/components/result/result.js
+++ src/components/result/result.js
@@ -11,33 +11,78 @@ const Results = () => {
 
   return (
     <div className="results">
-      <main className="result_main" >
-        <button className="close" id="close" onClick={onCloseClick}></button>
-        <h3 className="result_text" id="result_text" >Результаты</h3>
-        <div className="Inform">
-          <h2 className="text_namecomand" id="text_namecomand">Название команды</h2>
-          <h2 className="text_score" id="text_score">баллы</h2>
-        </div>
-          
-          <ol className="result_list">
-            <li className="result_list_elem">
-              <input className="list_inp text_comand" id="text_comand" type="text"  value={"Незабудки"}></input>
-              <input className="list_inp score" id="score" type="text" value={"33"}></input>
-            </li>
-
-            <li className="result_list_elem">
-              <input className="list_inp text_comand" id="text_comand" type="text"  value={"Кошатника"}></input>
-              <input className="list_inp score" id="score" type="text" value={"54"}></input>
-            </li>
-
-            <li className="result_list_elem">
-              <input className="list_inp text_comand" id="text_comand" type="text"  value={"Лучики"}></input>
-              <input className="list_inp score" id="score" type="text" value={"45"}></input>
-            </li>
-
-          
+      <main className="result_main">
         
-          </ol>
+        <button className="close" id="close" onClick={onCloseClick}></button>
+        <h3 className="result_text" id="result_text"> Результаты </h3>
+        
+        <div className="Inform">
+          <div>
+            <h2 className="text_namecomand" id="text_namecomand">Название команды</h2>
+            
+            <ol className="result_list">
+              <li className="result_list_elem">
+                <input
+                  className="list_inp text_comand"
+                  id="text_comand"
+                  type="text"
+                  value={"Незабудки"}
+                ></input>
+              </li>
+              
+              <li className="result_list_elem">
+                <input
+                  className="list_inp text_comand"
+                  id="text_comand"
+                  type="text"
+                  value={"Кошатника"}
+                ></input>
+              </li>
+              
+              <li className="result_list_elem">
+                <input
+                  className="list_inp text_comand"
+                  id="text_comand"
+                  type="text"
+                  value={"Лучики"}
+                ></input>
+              </li>
+            </ol>
+          </div>
+          
+          <div>
+            <h2 className="text_score" id="text_score">Баллы</h2>
+            
+            <ol className="result_list">
+              <li className="result_list_elem">
+                <input
+                  className="list_inp score"
+                  id="score"
+                  type="text"
+                  value={"33"}
+                ></input>
+              </li>
+              
+              <li className="result_list_elem">
+                <input
+                  className="list_inp score"
+                  id="score"
+                  type="text"
+                  value={"54"}
+                ></input>
+              </li>
+              
+              <li className="result_list_elem">
+                <input
+                  className="list_inp score"
+                  id="score"
+                  type="text"
+                  value={"45"}
+                ></input>
+              </li>
+            </ol>
+          </div>
+        </div>
       </main>
     </div>
   );
