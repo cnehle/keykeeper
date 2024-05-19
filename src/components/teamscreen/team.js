fc86ae2dd2daf984a83532c55fa79e1e04ca937e
_cnehle <emoskvina@sfedu.ru>
2024-05-19 04:11:37 +0300

    passed stations list added

--- src/components/teamscreen/team.js
+++ src/components/teamscreen/team.js
@@ -17,6 +17,21 @@ const TeamScreen = () => {
                     <h3 className="team_text" id="team_text">Команда:</h3>
                     <h3 className="team_name">Team 1</h3>
                 </div>
+
+                <div className="team_passed_stations_list">
+                    <h3 className="team_text team_pslist_text">Список пройденных станций:</h3>
+                    <ul className="team_pslist">
+                        <li className="team_pslist_elem">
+                            Station 1
+                        </li>
+                        <li className="team_pslist_elem">
+                            Station 2
+                        </li>
+                        <li className="team_pslist_elem">
+                            Station 3
+                        </li>
+                    </ul>
+                </div>
             </main>
         </div>
     );
