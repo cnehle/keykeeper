5f6ee4e1f68533d9d0b18c23117f35856a7348c7
AnyaKara <kassandra2701@yandex.ru>
2024-05-19 21:23:25 +0300

    Good admin

--- src/components/result/result.js
+++ src/components/result/result.js
@@ -16,72 +16,83 @@ const Results = () => {
         <button className="result_close" id="close" onClick={onCloseClick}></button>
         <h3 className="result_text" id="result_text"> Результаты </h3>
         
+        
+
         <div className="Inform">
-          <div>
-            <h2 className="text_namecomand" id="text_namecomand">Название команды</h2>
-            
-            <ol className="result_list">
-              <li className="result_list_elem">
-                <input
-                  className="list_inp text_comand"
-                  id="text_comand"
-                  type="text"
-                  value={"Незабудки"}
-                ></input>
-              </li>
-              
-              <li className="result_list_elem">
-                <input
-                  className="list_inp text_comand"
-                  id="text_comand"
-                  type="text"
-                  value={"Кошатники"}
-                ></input>
-              </li>
-              
-              <li className="result_list_elem">
-                <input
-                  className="list_inp text_comand"
-                  id="text_comand"
-                  type="text"
-                  value={"Лучики"}
-                ></input>
-              </li>
-            </ol>
-          </div>
+          <h2 className="text_namecomand" id="text_namecomand">Название команды</h2>
+          <h2 className="text_score" id="text_score">Баллы</h2>
+          <h2 className="text_time" >Время</h2>
           
-          <div>
-            <h2 className="text_score" id="text_score">Баллы</h2>
-            
-            <ol className="result_list">
+          <ol className="result_list">
               <li className="result_list_elem">
+                <input
+                  className="list_inp text_comand"
+                  id="text_comand"
+                  type="text"
+                  value={"Незабудки"}> 
+                </input>
+
                 <input
                   className="list_inp score"
                   id="score"
                   type="text"
-                  value={"33"}
-                ></input>
+                  value={"33"}>
+                </input>
+
+                <input
+                  className="list_inp time"
+                  type="text"
+                  value={"1:20"}>
+                </input>
               </li>
               
+
               <li className="result_list_elem">
+                <input
+                  className="list_inp text_comand"
+                  id="text_comand"
+                  type="text"
+                  value={"Незабудки"}> 
+                </input>
+
                 <input
                   className="list_inp score"
                   id="score"
                   type="text"
-                  value={"54"}
-                ></input>
+                  value={"33"}>
+                </input>
+
+                <input
+                  className="list_inp time"
+                  type="text"
+                  value={"1:20"}>
+                </input>
               </li>
-              
+
+
               <li className="result_list_elem">
+                <input
+                  className="list_inp text_comand"
+                  id="text_comand"
+                  type="text"
+                  value={"Незабудки"}> 
+                </input>
+
                 <input
                   className="list_inp score"
                   id="score"
                   type="text"
-                  value={"45"}
-                ></input>
+                  value={"33"}>
+                </input>
+
+                <input
+                  className="list_inp time"
+                  type="text"
+                  value={"1:20"}>
+                </input>
               </li>
-            </ol>
-          </div>
+          </ol>    
+
         </div>
       </main>
     </div>
