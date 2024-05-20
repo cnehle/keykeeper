c223833f41984c52cf263844f7c9f6f84fb4bec4
_cnehle <emoskvina@sfedu.ru>
2024-05-20 04:42:25 +0300

    stedit style modifs

--- src/components/stationeditscreen/stedit.js
+++ src/components/stationeditscreen/stedit.js
@@ -35,14 +35,14 @@ const StationEditScreen = () => {
         <div className="station_screen">
             <main className="station_screen_main">
                 <button className="station_screen_close" onClick={onCrossBtn2Click} />
-                <h2 className="text_comandname">Название станции:</h2>
-                <input className="screen_text_station" type="text" value={stationName} onChange={handleStnameChange} />
+                <h2 className="stname_text">Название станции:</h2>
+                <input className="stname" type="text" value={stationName} onChange={handleStnameChange} />
 
                 <h2 className="curator_password_text">Пароль для куратора станции:</h2>
                 <input className="curator_password" value={curpassword} onChange={handlePswChange} />
 
                 <h2 className="rules_text">Правила станции:</h2>
-                <textarea className="description" rows="5" cols="50" value={rules} onChange={handleRulesChange} />
+                <textarea className="rules" rows="5" cols="50" value={rules} onChange={handleRulesChange} />
 
                 <button className="btn_station_screen save" onClick={onSaveClick}>Сохранить</button>
             </main>
