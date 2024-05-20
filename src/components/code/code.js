33eb6025b75b8a8cc57e86651ea14fed51ffc9fa
_cnehle <emoskvina@sfedu.ru>
2024-05-20 15:52:11 +0300

    curator functions added

--- src/components/code/code.js
+++ src/components/code/code.js
@@ -1,11 +1,16 @@
 
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import '../code/code.css';
 
 function Code() {
   const navigate = useNavigate();
   const location = useLocation();
+  const [password, setPassword] = useState("");
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
   const onNextClick = useCallback(() => {
     const accessCode = document.getElementsByClassName("input")[0].value;
@@ -15,8 +20,9 @@ function Code() {
     }
     else if (accessCode === "keykeeper" && btnId === "1") {
       navigate('/teamedit');
-    } else if (accessCode === "curator" && btnId === "2") {
-      navigate('/curator');
+    } else if (btnId === "2") {
+      const psw = password;
+      navigate('/curator', { state: psw });
     }
     else {
       alert("Некорректный пароль");
@@ -28,7 +34,7 @@ function Code() {
       <main className="code_main">
 
         <h2 className="code_text">КОД ДОСТУПА:</h2>
-        <input className="input" type="password" />
+        <input className="input" type="password" value={password} onChange={handlePasswordChange} />
         <button className="btn_next" onClick={onNextClick}>Далее</button>
 
       </main>
--- src/components/curator/curator.js
+++ src/components/curator/curator.js
@@ -1,11 +1,14 @@
-import { useCallback, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useCallback, useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import "../curator/curator.css";
 
 const CuratorScreen = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const [qteam, setQteam] = useState("");
     const [score, setScore] = useState("");
+    const [stName, setStName] = useState("");
+    const psw = location.state;
 
     const onCloseClick = useCallback(() => {
         navigate("/");
@@ -19,20 +22,141 @@ const CuratorScreen = () => {
         setScore(e.target.value);
     };
 
+    useEffect(() => {
+        const url = 'https://warthog-growing-honeybee.ngrok-free.app/API/v1/station/token';
+
+        fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'ngrok-skip-browser-warning': 'true'
+            },
+            password: psw
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setStName(data.map(item => item.name));
+            })
+            .catch(error => {
+                console.error('Ошибка при получении данных:', error);
+            });
+    }, []);
+
+    const onStartClClick = async () => {
+        const url = 'https://warthog-growing-honeybee.ngrok-free.app/API/v1/challenge/start';
+
+        const new_team = {
+            team_name: qteam
+        };
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Token': 'qDpIsiaWmnIb',
+            'ngrok-skip-browser-warning': 'true'
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: headers,
+                body: JSON.stringify(new_team)
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                alert(`Ответ сервера: ${JSON.stringify(data)}`);
+            } else {
+                const error = await response.json();
+                alert(`Ошибка: ${error.error}`);
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
+    };
+
+    const onEndClClick = async () => {
+        const url = 'https://warthog-growing-honeybee.ngrok-free.app/API/v1/challenge/end';
+
+        const new_team = {
+            team_name: qteam
+        };
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Token': 'qDpIsiaWmnIb',
+            'ngrok-skip-browser-warning': 'true'
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: headers,
+                body: JSON.stringify(new_team)
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                alert(`Ответ сервера: ${JSON.stringify(data)}`);
+            } else {
+                const error = await response.json();
+                alert(`Ошибка: ${error.error}`);
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
+    };
+
+    const onOkClick = async () => {
+        const url = 'https://warthog-growing-honeybee.ngrok-free.app/API/v1/challenge/rate';
+
+        const new_score = {
+            team_name: qteam,
+            score: score
+        };
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Token': 'qDpIsiaWmnIb',
+            'ngrok-skip-browser-warning': 'true'
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: headers,
+                body: JSON.stringify(new_score)
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                alert(`Ответ сервера: ${JSON.stringify(data)}`);
+            } else {
+                const error = await response.json();
+                alert(`Ошибка: ${error.error}`);
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
+    };
+
     return (
         <div className="curator-screen">
             <main className="curator_main">
 
                 <div className="curator_header">
                     <button className="teamedit_close" id="close" onClick={onCloseClick}></button>
-                    <h1 className="curator_text station_name">Название станции</h1>
+                    <h1 className="curator_text station_name">{stName}</h1>
                     <input className="t_name" value={qteam} onChange={handleQteamChange} />
-                    <button className="begin_challenge">Начать испытание</button>
-                    <button className="end_challendge">Закончить испытание</button>
+                    <button className="begin_challenge" onClick={onStartClClick}>Начать испытание</button>
+                    <button className="end_challendge" onClick={onEndClClick}>Закончить испытание</button>
                     <div className="creative">
                         <h3 className="creative_text">Баллы за креатив:</h3>
                         <input className="creative_score" value={score} onChange={handleScoreChange} />
-                        <button className="ok_score">ОК</button>
+                        <button className="ok_score" onClick={onOkClick}>ОК</button>
                     </div>
 
                 </div>
