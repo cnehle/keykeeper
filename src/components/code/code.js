131fec7bdadca10d63226ad071682810ee5c80e5
_cnehle <emoskvina@sfedu.ru>
2024-05-20 15:06:08 +0300

    code acces and team edit added

--- src/components/code/code.js
+++ src/components/code/code.js
@@ -1,26 +1,39 @@
 
 import { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import '../code/code.css';
 
 function Code() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const onNextClick = useCallback(() => {
-    navigate('/admin');
-  }, [navigate]);
+    const accessCode = document.getElementsByClassName("input")[0].value;
+    const btnId = location.state;
+    if (accessCode === "admin" && btnId === "0") {
+      navigate('/admin');
+    }
+    else if (accessCode === "keykeeper" && btnId === "1") {
+      navigate('/teamedit');
+    } else if (accessCode === "curator" && btnId === "2") {
+      navigate('/curator');
+    }
+    else {
+      alert("Некорректный пароль");
+    }
+  }, [navigate, location.state]);
 
   return (
     <div className="code">
       <main className="code_main">
-        
+
         <h2 className="code_text">КОД ДОСТУПА:</h2>
         <input className="input" type="password" />
         <button className="btn_next" onClick={onNextClick}>Далее</button>
-       
+
       </main>
     </div>
-);
+  );
 };
 
 export default Code;
--- src/components/teameditscreen/teamedit.js
+++ src/components/teameditscreen/teamedit.js
@@ -19,36 +19,56 @@ const TeamEditScreen = () => {
         setTeamPassword(e.target.value);
     };
 
-    const handleSaveClick = () => {
+    const handleSaveClick = async () => {
+        const url = 'https://warthog-growing-honeybee.ngrok-free.app/API/v1/team/add';
+
         const new_team = {
             name: teamName,
             password: teamPassword,
             teammates: []
         };
 
-        const Http = new XMLHttpRequest();
-        const url = 'http://5.101.7.212:8181/API/v1/team/add';
-        Http.open("POST", url);
-        Http.setRequestHeader("Content-Type", "application/json");
-        Http.send(JSON.stringify(new_team));
+        const headers = {
+            'Content-Type': 'application/json',
+            'Token': 'qDpIsiaWmnIb',
+            'ngrok-skip-browser-warning': 'true'
+        };
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(new_team)
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                alert(`Ответ сервера: ${JSON.stringify(data)}`);
+            } else {
+                const error = await response.json();
+                alert(`Ошибка: ${error.error}`);
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
     };
 
     return (
         <div className="teamedit">
-        <main className="teamedit_main">
-            <div className="teamedit_header">
-                <button className="teamedit_close" id="close" onClick={onCloseClick}></button>
-                <h3 className="teamedit_text">Название команды:</h3>
-                <input className="set_team_name" value={teamName} onChange={handleTeamNameChange} />
-                <h3 className="teamedit_text teamedit_password">Пароль:</h3>
-                <input className="set_team_password" value={teamPassword} onChange={handleTeamPasswordChange} />
-            </div>
+            <main className="teamedit_main">
+                <div className="teamedit_header">
+                    <button className="teamedit_close" id="close" onClick={onCloseClick}></button>
+                    <h3 className="teamedit_text">Название команды:</h3>
+                    <input className="set_team_name" value={teamName} onChange={handleTeamNameChange} />
+                    <h3 className="teamedit_text teamedit_password">Пароль:</h3>
+                    <input className="set_team_password" value={teamPassword} onChange={handleTeamPasswordChange} />
+                </div>
 
-            <div className="teamedit_footer">
-                <button className="teamedit_save_btn" onClick={handleSaveClick}>Сохранить</button>
-            </div>
-        </main>
-    </div>
+                <div className="teamedit_footer">
+                    <button className="teamedit_save_btn" onClick={handleSaveClick}>Сохранить</button>
+                </div>
+            </main>
+        </div>
     );
 };
 
